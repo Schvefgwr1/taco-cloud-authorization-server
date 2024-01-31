20e025a4d9ab14a7cd7ff9ce8665274ac39315ec
Schvefgwr1 <92165301+Schvefgwr1@users.noreply.github.com>
2024-01-31 22:12:38 +0300

    Initial commit

---
+++ src/main/java/sia/tacocloudauthorizationserver/TacoCloudAuthorizationServerApplication.java
@@ -0,0 +1,13 @@
+package sia.tacocloudauthorizationserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoCloudAuthorizationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacoCloudAuthorizationServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sia/tacocloudauthorizationserver/TacoCloudAuthorizationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package sia.tacocloudauthorizationserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoCloudAuthorizationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
